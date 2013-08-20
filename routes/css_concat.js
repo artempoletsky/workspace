288bee4551c6ddf30478b94e78e6d8432e285575
Artem Poletsky <artempoletsky@gmail.com>
2013-08-20 20:12:56 +0600

    css concat start

---
+++ routes/css_concat.js
@@ -0,0 +1,20 @@
+(function(){
+	"use strict";
+	
+	var fs=require('fs');
+	var files=['css/1.css', 'css/2.css'];
+	var target='result.css';
+	
+	var concated='', content, imgcount=0;
+	for(var i=0;i<files.length; i++){
+	content='';
+		try{
+			content=fs.readFileSync(files[i]);	
+		}catch(e){
+			console.log(e.message);
+		}
+		
+		concated+=content;
+	}
+	console.log(concated);
+}());
