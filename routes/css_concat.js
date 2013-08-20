3215e4fa40c139796b0c2b657f756e93083f85ac
Artem Poletsky <artempoletsky@gmail.com>
2013-08-20 21:30:05 +0600

    css concat prototype

--- routes/css_concat.js
+++ routes/css_concat.js
@@ -1,19 +1,56 @@
 (function(){
 	"use strict";
 	
-	var fs=require('fs');
 	var files=['css/1.css', 'css/2.css'];
-	var target='result.css';
+	var target='./result.css';
+	
+	var imagesFolder='img';
+	
+	
+	var fs=require('fs');
+	var util=require('util');
+	
+	
+	
+	
+	var copy=function(inFile, outFile){
+		util.debug('copy '+inFile+' '+outFile);
+		fs.writeFileSync(outFile, fs.readFileSync(inFile));		
+	};
+	
+	var dirname=function(path){
+		return path.replace(/(.*\/)[^\/\\]+$/, '$1');
+	}
+	
+	var extension=function(path){		
+		return /[^\.]+$/.exec(path)[0];
+	}
+	
+	var targetDir=dirname(target);
+	
+	
+	fs.mkdirSync(targetDir+imagesFolder);
 	
 	var concated='', content, imgcount=0;
 	for(var i=0;i<files.length; i++){
 	content='';
+	var currentCssName=files[i];
 		try{
-			content=fs.readFileSync(files[i]);	
+			content=fs.readFileSync(currentCssName);	
 		}catch(e){
 			console.log(e.message);
 		}
-		
+		var currentDir=dirname(currentCssName);
+		content=String(content).replace(/url\(['"]?(.*?)['"]?\)/g, function(match, url){
+			var newName=imagesFolder+'/'+imgcount+'.'+extension(url);
+			var oldPath=currentDir+url;
+			if(fs.existsSync(oldPath)){
+			imgcount++;
+			copy(oldPath,targetDir+newName)
+		return 'url('+newName+')';
+			}
+			return match;
+		});
 		concated+=content;
 	}
 	fs.writeFileSync(target, concated);
